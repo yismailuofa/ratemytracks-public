6c4c2065b774eab096940ff289145a357080d026
Youssef Ismail <yismail@ualberta.ca>
2021-07-15 23:34:50 -0600

    first draft main js

--- chromeExtension/ratemytracks/main.js
+++ chromeExtension/ratemytracks/main.js
@@ -1,1 +1,12 @@
-console.log("JOON")
+instructors = document.getElementsByClassName("ps_grid-col INSTRUCTOR")
+
+if (instructors.length) {
+    table = document.getElementsByClassName("ps_grid-flex")[0]
+    head = table.children[0].children[0]
+    body = table.children[1].children[0]
+
+    ratingHeader = head.insertCell(7)
+
+    // ratingHeader.innerHTML =
+
+}
